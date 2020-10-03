4cb51edd8501f5ad32fc782e9d6a4f52b41db32f
Nguyen Van Dai <nguyenvandaibe@gmail.com>
2020-10-03 16:47:47 +0700

    Ví dụ regex

--- test/routes/web.php
+++ test/routes/web.php
@@ -11,8 +11,25 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('tim-kiem/{search}', function ($search) {
+
+    // Tìm theo tỉnh thành
+    // tim-kiem/nha-dat-ban_ha-noi
+    if (preg_match('/^[a-z0-9-]+$/', $search)) {
+        dd('Menu: ' . $search);
+    }
+
+    // Tìm theo menu
+    // tim-kiem/nha-dat-ban
+    elseif (preg_match('/^[a-z0-9-]+_[a-z0-9-]+$/', $search)) {
+        dd('Province: ' . $search);
+    } else {
+        dd('None');
+    }
 });
